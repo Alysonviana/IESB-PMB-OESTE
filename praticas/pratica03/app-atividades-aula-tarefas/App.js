b2786736c65be13f349116c2d9c194d80feac78d
unknown <24214290012@ACADEMICO.IESB.BR>
2026-08-31 21:23:42 -0300

    feat: atualização

--- praticas/pratica03/app-atividades-aula-tarefas/App.js
+++ praticas/pratica03/app-atividades-aula-tarefas/App.js
@@ -6,11 +6,25 @@ import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_listas_metas} from 
 
 export default function App() {
   return (
-    <View style={styles.mainContainer}> 
-      <TextInput style={[styles.padrao, styles.primari1]} placeholder={rotulo_input_meta}/>
-      <Button style={styles.padrao} title={rotulo_btn_cadastro_meta}/>
-      <Text style={styles.padrao}>{rotulo_listas_metas}</Text>
-      <StatusBar style="auto" />,
+    <View style={styles.mainContainer}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', }}>
+        <View style={{ width: '65%' }}>
+          <TextInput
+            style={[styles.padrao, styles.primari1]}
+            placeholder={rotulo_input_meta}
+          />
+        </View>
+        <View style={{ width: '30%',}}>
+          <Button
+            style={styles.padrao}
+            title={rotulo_btn_cadastro_meta}
+          />
+        </View>
+      </View>
+      <View>
+        <Text style={styles.padrao}>{rotulo_listas_metas}</Text>
+      </View>
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -24,14 +38,15 @@ const styles = StyleSheet.create({
   },
 
   mainContainer:{
-    margin:30,
+    margin:20,
+    marginTop:55,
   },
 
   padrao:{
-    margin: 20,
+  marginBottom: 10,
   },
 
-  primari1:{
+  primari1 :{
     borderColor:'#cccccc',
     borderWidth: 2,
     borderRadius: 10,
