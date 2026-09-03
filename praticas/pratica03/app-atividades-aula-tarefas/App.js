cd4d9a30564157fcbca02eb0fc30435a1367e3dc
unknown <24214290012@ACADEMICO.IESB.BR>
2026-09-02 21:40:15 -0300

    feat:atualizacao

--- praticas/pratica03/app-atividades-aula-tarefas/App.js
+++ praticas/pratica03/app-atividades-aula-tarefas/App.js
@@ -1,27 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button ,StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 import { titulo, titulo_padrao } from './util';
-import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_listas_metas} from './mensagens';
+import { rotulo_btn_cadastro_meta, rotulo_input_meta, rotulo_listas_metas } from './mensagens';
 
 
 export default function App() {
   return (
     <View style={styles.mainContainer}>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', }}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', alignItems: 'baseline', flex: 1 }}>
         <View style={{ width: '65%' }}>
           <TextInput
             style={[styles.padrao, styles.primari1]}
             placeholder={rotulo_input_meta}
           />
         </View>
-        <View style={{ width: '30%',}}>
+        <View style={{ width: '30%', }}>
           <Button
             style={styles.padrao}
             title={rotulo_btn_cadastro_meta}
           />
         </View>
       </View>
-      <View>
+      <View style={styles.metaContainer}>
         <Text style={styles.padrao}>{rotulo_listas_metas}</Text>
       </View>
       <StatusBar style="auto" />
@@ -31,26 +31,32 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  mainContainer:{
-    margin:20,
-    marginTop:55,
+  mainContainer: {
+    marginTop: 15,
+    padding: 30,
+    flex: 1,
+    flexDirection: 'column',
   },
 
-  padrao:{
-  marginBottom: 10,
+  padrao: {
+    marginBottom: 10,
   },
 
-  primari1 :{
-    borderColor:'#cccccc',
+  primari1: {
+    borderColor: '#cccccc',
     borderWidth: 2,
     borderRadius: 10,
     backgroundColor: 'rgb(227, 228, 227)'
+  },
+
+  metaContainer:{
+  flex: 5,
   }
 
 });
+
+
